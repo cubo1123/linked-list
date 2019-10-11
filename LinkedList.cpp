e19dfa2991f8eb90c025bcbf0998671f45979ab0
Enrique Perez <cubo.enrique@gmail.com>
2019-10-11 14:12:54 -0500

    add nodes to the linked list

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -5,3 +5,17 @@ LinkedList::LinkedList(){
     this->first = nullptr;
     this->last = nullptr;
 }
+
+void LinkedList::add(string data,int id){
+    Node* node = new Node();
+    node->name = data;
+    node->id = id;
+    node->next = nullptr;
+    if(this->first == nullptr){
+        this->first = node;
+    }else{
+        this->last->next = node;
+    }
+    this->last = node;
+    this->length++;
+}
